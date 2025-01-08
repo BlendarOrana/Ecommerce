78b6df4a7beb7ac30b66a989767b9519ac694964
BlendarOrana <blendorana1@hotmail.com>
2025-01-08 19:54:53 +0100

    protect route added

--- backend/middleware/route.middleware.js
+++ backend/middleware/route.middleware.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 import  User  from '../models/user.model.js';
 import dotenv from 'dotenv';
+import e from 'express';
 dotenv.config();
 
 
@@ -13,15 +14,41 @@ export const protectRoute = async (req, res, next) => {
             return res.status(401).json({ message: "no token" });
         }
 
-        const decoded  = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
-        const user = await User.findById(decoded.userId).select("-password");
+   try {
+    const decoded  = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decoded.userId).select("-password");
 
-        req.userId = user;
-        
-        next();
-    } catch (error) {
+    if (!user) {
+        return res.status(401).json({ message: "User not found" });
+    }
+
+    req.userId = user;
+    
+    next();
+    
+   } catch (error) {
+    if(error.name === "TokenExpiredError"){
+        return res.status(401).json({message:"Token expired"})
+    }
+    throw error
+   }
+
+    }
+    
+    catch (error) {
         console.log("Error in protectRoute", error.message);
         res.status(401).json({ message: "Unauthorized" });
         
     }
+}
+
+export const adminRoute = async (req, res, next) => {
+
+if(req.user && req.user.role === "admin"){
+    next();
+
+}
+else{
+    return res.status(403).json({message:"Not authorized as an admin"})
+}
 }
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { getAllProducts } from "../controllers/product.controller.js";
+import { adminRoute, protectRoute } from "../middleware/route.middleware.js";
 
 const router = express.Router();
 
